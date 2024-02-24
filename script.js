2b7255067172f5caa09d601e20af94a2f823ba97
mohammed kaif <mkaif555123@gmail.com>
2024-02-24 00:29:41 -0800

    JS ASSAIGMENT

---
+++ script.js
@@ -0,0 +1,6 @@
+let a = +prompt("ENTER VALUE 1");
+let b = +prompt("ENTER VALUE 2");
+document.write("ADITTION =", " ", a + b, "<br>");
+document.write("SUBTRACTION =", " ", a - b, "<br>");
+document.write("MULTIPLICATION =", " ", a * b, "<br>");
+document.write("DIVISION =", " ", a / b, "<br>");
